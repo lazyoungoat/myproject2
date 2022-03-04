bd65c55821d83b68b7454cb3b2962867d05b3825
Brian <l.r.shiun@gmail.com>
2022-03-04 12:54:49 +0800

    commit

---
+++ src/main/java/com/brian/Scoring.java
@@ -0,0 +1,13 @@
+package com.brian;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student ben = new Student();
+        ben.name = "Ben";
+        ben.english = 34;
+        ben.math = 81;
+        System.out.println(ben.english);
+
+
+    }
+}
---
+++ src/main/java/com/brian/Student.java
@@ -0,0 +1,8 @@
+package com.brian;
+
+public class Student {
+    String name;
+    int math;
+    int english;
+
+}
