cce24f40685dc876be0d57c5941af8148f472261
Nikita <l.r.shiun@gmail.com>
2022-04-04 01:03:14 +0800

    commit 1

--- src/main/java/com/nikita/ticket/Tester.java
+++ src/main/java/com/nikita/ticket/Tester.java
@@ -9,10 +9,15 @@ public class Tester {
         System.out.println(ticket.start.id);
 
 
+        System.out.println("Please choose a starting station:(Taipei:1, Taichung:2, Kaohsiung:3)");
+        Scanner scan_start = new Scanner(System.in);
+        String str_start = scan_start.next();
+        int start = Integer.parseInt(str_start);
 
-        System.out.println("Please choose a station:(Taipei, Taichung, Kaohsiung)");
-        Scanner scanner = new Scanner(System.in);
-        int station = Integer.parseInt(scanner.next());
+        System.out.println("Please choose a ending station:(Taipei:1, Taichung:2, Kaohsiung:3)");
+        Scanner scan_end = new Scanner(System.in);
+        String str_end = scan_end.next();
+        int end = Integer.parseInt(str_end);
 
 
     }
