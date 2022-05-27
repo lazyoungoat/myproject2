8350d7c372c2e2d60c5759512b930f3c8c5852e7
Nikita <l.r.shiun@gmail.com>
2022-05-27 15:24:16 +0800

    change label to number your guess

--- src/main/java/com/nikita/ui/GuessFrame.java
+++ src/main/java/com/nikita/ui/GuessFrame.java
@@ -7,6 +7,9 @@ import java.awt.event.ActionListener;
 
 public class GuessFrame extends JFrame {
     //Fields
+    JButton button = new JButton("Guess");
+    JTextField number = new JTextField(10);
+    JLabel label = new JLabel("number");
 
     //constructor
     public GuessFrame(){
@@ -14,17 +17,22 @@ public class GuessFrame extends JFrame {
         setSize(500,300);
         setLocation(444,245);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JButton button = new JButton("boom");
-        JLabel label = new JLabel("come");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 //                System.out.println("Hehe");
-                label.setText("Hehe");
+                int num = Integer.parseInt(number.getText());
+                System.out.println(num);
+                label.setText("hehe");
+                label.setText("number: " + num);
             }
         });
 
-
+        setLayout(new FlowLayout());
+        add(number);
+        add(label);
+        add(button);
+//        label.setText("know");
         setVisible(true);
 
     }
