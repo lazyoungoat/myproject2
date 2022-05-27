ae362d09aa19a5cb72038e72d47efff482404cb8
Nikita <l.r.shiun@gmail.com>
2022-05-27 19:41:54 +0800

    Guess 1~10 in Frame

--- src/main/java/com/nikita/ui/GuessFrame.java
+++ src/main/java/com/nikita/ui/GuessFrame.java
@@ -1,15 +1,23 @@
 package com.nikita.ui;
 
+import org.w3c.dom.ls.LSOutput;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
     //Fields
     JButton button = new JButton("Guess");
     JTextField number = new JTextField(10);
     JLabel label = new JLabel("number");
+    Random random = new Random();
+    int secret = random.nextInt(10+1);
+
+
+
 
     //constructor
     public GuessFrame(){
@@ -17,14 +25,22 @@ public class GuessFrame extends JFrame {
         setSize(500,300);
         setLocation(444,245);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        System.out.println(secret);
+
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 //                System.out.println("Hehe");
                 int num = Integer.parseInt(number.getText());
                 System.out.println(num);
-                label.setText("hehe");
                 label.setText("number: " + num);
+
+                if (num>secret)
+                    label.setText("Smaller");
+                else if (num<secret)
+                    label.setText("Bigger");
+                else
+                    label.setText("Congratulations");
             }
         });
 
