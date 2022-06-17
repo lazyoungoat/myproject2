fc94401d8c17563c4fd16cdb6ae4a5a3032e509c
Nikita <l.r.shiun@gmail.com>
2022-06-17 13:02:03 +0800

    Java BufferedReader

--- src/main/java/com/nikita/score/ScoringFile.java
+++ src/main/java/com/nikita/score/ScoringFile.java
@@ -1,5 +1,6 @@
 package com.nikita.score;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,11 +9,25 @@ public class ScoringFile {
     public static void main(String[] args) {
         try {
             FileReader fileReader = new FileReader("scores.txt");
-            int data = fileReader.read();
-            while (data != -1){
-                System.out.print((char) data);
-                data = fileReader.read();
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = reader.readLine();
+            while (line != null){
+//                System.out.println(line);
+                String[] token = line.split(",");
+                String name = token[0];
+                int english = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Graduate stu = new Graduate(name, english, math);
+                stu.print();
+                line = reader.readLine();
             }
+
+
+            /*int data = fileReader.read();
+            while (data != -1){
+                System.out.println((char) data);
+                data = fileReader.read();
+            }*/
         }catch (FileNotFoundException e){
             System.out.println("File not found");
         }catch (IOException e){
